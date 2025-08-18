813b367e834b22e01cd179b65b67f1f9d9d7fd3f
Arun Kumar Bhagat <arunbhagat23bai70142@gmail.com>
2025-08-18 14:04:54 +0530

    Time: 147 ms (87.48%) | Memory: 24.3 MB (93.89%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        n = len(temperatures)
+        answer = [0] * n
+        stack = []  # stores indices
+
+        for i, temp in enumerate(temperatures):
+            # check if today is warmer than a previous day
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                prev_index = stack.pop()
+                answer[prev_index] = i - prev_index
+            stack.append(i)
+
+        return answer
+    
