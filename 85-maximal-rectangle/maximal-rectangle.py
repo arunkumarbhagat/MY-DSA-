843e1c5868180b252cc4210f186201753c180e03
Arun Kumar Bhagat <arunbhagat23bai70142@gmail.com>
2026-01-14 14:24:42 +0530

    Time: 58 ms (31.50%) | Memory: 18.4 MB (8.94%) - LeetSync

---
+++ 85-maximal-rectangle/maximal-rectangle.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maximalRectangle(self, matrix):
+        if not matrix:
+            return 0
+
+        cols = len(matrix[0])
+        heights = [0] * cols
+        max_area = 0
+
+        for row in matrix:
+            for i in range(cols):
+                heights[i] = heights[i] + 1 if row[i] == '1' else 0
+
+            stack = []
+            for i in range(cols + 1):
+                curr = heights[i] if i < cols else 0
+                while stack and heights[stack[-1]] > curr:
+                    h = heights[stack.pop()]
+                    w = i if not stack else i - stack[-1] - 1
+                    max_area = max(max_area, h * w)
+                stack.append(i)
+
+        return max_area
