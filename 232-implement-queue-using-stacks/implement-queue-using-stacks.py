4e837cf6b51394dbc3a69e6dab0d2a07135ecba2
Arun Kumar Bhagat <arunbhagat23bai70142@gmail.com>
2026-01-14 10:00:49 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (60.65%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.py
@@ -0,0 +1,25 @@
+class MyQueue:
+
+    def __init__(self):
+        self.inStack = []
+        self.outStack = []
+
+    def push(self, x):
+        self.inStack.append(x)
+
+    def pop(self):
+        self._move_if_needed()
+        return self.outStack.pop()
+
+    def peek(self):
+        self._move_if_needed()
+        return self.outStack[-1]
+
+    def empty(self):
+        return not self.inStack and not self.outStack
+
+    def _move_if_needed(self):
+        if not self.outStack:
+            while self.inStack:
+                self.outStack.append(self.inStack.pop())
+
